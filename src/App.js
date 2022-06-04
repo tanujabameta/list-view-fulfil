3f76cac63335605588663d02ee1e787152b0b464
Tanuja Bameta <16jantanuja.bameta@gmail.com>
2022-06-04 15:19:00 +0530

    initial repo setup with table and data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import DataTable from "./Components/DataTable/DataTable";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const result = await axios(
+        "https://jsonplaceholder.typicode.com/photos/"
+      );
+      setData(result.data);
+    })();
+  }, []);
+
+  const columns = [
+    { id: "albumId", label: "Album ID", numeric: true },
+    { id: "title", label: "Title", numeric: false },
+    { id: "Url", label: "Url", numeric: false },
+    { id: "thumbnailUrl", label: "ThumbNail Url", numeric: false },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>List View</header>
+      <DataTable data={data} columns={columns} />
     </div>
   );
 }
---
+++ src/Components/DataTable/DataTable.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./DataTable.css";
+
+const DataTable = ({ data, columns }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>
+            <input type="checkbox" />
+          </th>
+          {columns.map((column) => {
+            return <th key={column.id}>{column.label}</th>;
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item) => {
+          return (
+            <tr key={item.id}>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>{item.albumId}</td>
+              <td>{item.title}</td>
+              <td><img className="url" src={item.url} alt="url"/></td>
+              <td><img className="thumbnailUrl" src={item.thumbnailUrl} alt="thumbnail Url"/></td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default DataTable;
